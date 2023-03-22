0e3dc4faf186aac08a332c16932066c35bbd775e
FreePhoenix888 <freephoenix888@gmail.com>
2023-03-23 00:44:01 +0600

    Add code from GPT4

--- src/trees/no_recur_szb_tree.rs
+++ src/trees/no_recur_szb_tree.rs
@@ -1,153 +1,147 @@
 use platform_data::LinkType;
-
 use crate::SzbTree;
 
 pub trait NoRecurSzbTree<T: LinkType>: SzbTree<T> {
-    unsafe fn attach(&mut self, root: *mut T, node: T) {
-        if *root == T::funty(0) {
+    fn attach(&mut self, root: &mut Option<T>, node: T) {
+        if root.is_none() {
             self.set_size(node, T::funty(1));
-            *root = node;
+            *root = Some(node);
             return;
         }
         self.attach_core(root, node);
     }
-    unsafe fn detach(&mut self, root: *mut T, node: T) {
+    fn detach(&mut self, root: &mut Option<T>, node: T) {
         self.detach_core(root, node);
     }
 
-    unsafe fn attach_core(&mut self, mut root: *mut T, node: T) {
+    fn attach_core(&mut self, root: &mut Option<T>, node: T) {
         loop {
-            let left = self.get_mut_left_reference(*root);
-            let left_size = self.get_size_or_zero(*left);
-            let right = self.get_mut_right_reference(*root);
-            let right_size = self.get_size_or_zero(*right);
-            if self.first_is_to_the_left_of_second(node, *root) {
-                if *left == T::funty(0) {
-                    self.inc_size(*root);
+            let left = self.get_mut_left_reference(root.as_ref().unwrap());
+            let left_size = self.get_size_or_zero(left.as_ref());
+            let right = self.get_mut_right_reference(root.as_ref().unwrap());
+            let right_size = self.get_size_or_zero(right.as_ref());
+            if self.first_is_to_the_left_of_second(node, root.as_ref().unwrap()) {
+                if left.is_none() {
+                    self.inc_size(root.as_mut().unwrap());
                     self.set_size(node, T::funty(1));
-                    *left = node;
+                    *left = Some(node);
                     return;
                 }
-                if self.first_is_to_the_left_of_second(node, *left) {
+                if self.first_is_to_the_left_of_second(node, left.as_ref().unwrap()) {
                     if (left_size + T::funty(1)) > right_size {
                         self.right_rotate(root);
                     } else {
-                        self.inc_size(*root);
+                        self.inc_size(root.as_mut().unwrap());
                         root = left;
                     }
                 } else {
-                    let left_right_size = self.get_size_or_zero(self.get_right(*left));
+                    let left_right_size = self.get_size_or_zero(self.get_right(left.as_ref().unwrap()));
                     if (left_right_size + T::funty(1)) > right_size {
                         if left_right_size == T::funty(0) && right_size == T::funty(0) {
-                            self.set_left(node, *left);
-                            self.set_right(node, *root);
+                            self.set_left(node, left.take().unwrap());
+                            self.set_right(node, root.take().unwrap());
                             self.set_size(node, left_size + T::funty(1) + T::funty(1));
-                            self.set_left(*root, T::funty(0));
-                            self.set_size(*root, T::funty(1));
-                            *root = node;
+                            self.set_left(root.as_mut().unwrap(), T::funty(0));
+                            self.set_size(root.as_mut().unwrap(), T::funty(1));
+                            *root = Some(node);
                             return;
                         }
                         self.left_rotate(left);
                         self.right_rotate(root);
                     } else {
-                        self.inc_size(*root);
+                        self.inc_size(root.as_mut().unwrap());
                         root = left;
                     }
                 }
             } else {
-                if *right == T::funty(0) {
-                    self.inc_size(*root);
+                if right.is_none() {
+                    self.inc_size(root.as_mut().unwrap());
                     self.set_size(node, T::funty(1));
-                    *right = node;
+                    *right = Some(node);
                     return;
                 }
-                if self.first_is_to_the_right_of_second(node, *right) {
+                if self.first_is_to_the_right_of_second(node, right.as_ref().unwrap()) {
                     if (right_size + T::funty(1)) > left_size {
                         self.left_rotate(root);
                     } else {
-                        self.inc_size(*root);
+                        self.inc_size(root.as_mut().unwrap());
                         root = right;
                     }
                 } else {
-                    let right_left_size = self.get_size_or_zero(self.get_left(*right));
+                    let right_left_size = self.get_size_or_zero(self.get_left(right.as_ref().unwrap()));
                     if (right_left_size + T::funty(1)) > left_size {
                         if right_left_size == T::funty(0) && left_size == T::funty(0) {
-                            self.set_left(node, *root);
-                            self.set_right(node, *right);
-                            self.set_size(node, right_size + T::funty(1) + T::funty(1));
-                            self.set_right(*root, T::funty(0));
-                            self.set_size(*root, T::funty(1));
-                            *root = node;
-                            return;
-                        }
-                        self.right_rotate(right);
-                        self.left_rotate(root);
-                    } else {
-                        self.inc_size(*root);
-                        root = right;
-                    }
-                }
-            }
-        }
-    }
-
-    unsafe fn detach_core(&mut self, mut root: *mut T, node: T) {
-        loop {
-            let left = self.get_mut_left_reference(*root);
-            let left_size = self.get_size_or_zero(*left);
-            let right = self.get_mut_right_reference(*root);
-            let right_size = self.get_size_or_zero(*right);
-            if self.first_is_to_the_left_of_second(node, *root) {
-                let decremented_left_size = left_size - T::funty(1);
-                if self.get_size_or_zero(self.get_right_or_default(*right)) > decremented_left_size
-                {
-                    self.left_rotate(root);
-                } else if self.get_size_or_zero(self.get_left_or_default(*right))
-                    > decremented_left_size
-                {
-                    self.right_rotate(right);
-                    self.left_rotate(root);
-                } else {
-                    self.dec_size(*root);
-                    root = left;
-                }
-            } else if self.first_is_to_the_right_of_second(node, *root) {
-                let decremented_right_size = right_size - T::funty(1);
-                if self.get_size_or_zero(self.get_left_or_default(*left)) > decremented_right_size {
-                    self.right_rotate(root);
-                } else if self.get_size_or_zero(self.get_right_or_default(*left))
-                    > decremented_right_size
-                {
-                    self.left_rotate(left);
-                    self.right_rotate(root);
-                } else {
-                    self.dec_size(*root);
-                    root = right;
-                }
+                            self.set_left(node, root.take().unwrap());
+                            self.set_right(node, right.take().unwrap());
+                            self.set_size(node, right_size +
+T::funty(1) + T::funty(1));
+self.set_right(root.as_mut().unwrap(), T::funty(0));
+self.set_size(root.as_mut().unwrap(), T::funty(1));
+*root = Some(node);
+return;
+}
+self.right_rotate(right);
+self.left_rotate(root);
+} else {
+self.inc_size(root.as_mut().unwrap());
+root = right;
+}
+}
+}
+}
+}
+fn detach_core(&mut self, root: &mut Option<T>, node: T) {
+    loop {
+        let left = self.get_mut_left_reference(root.as_ref().unwrap());
+        let left_size = self.get_size_or_zero(left.as_ref());
+        let right = self.get_mut_right_reference(root.as_ref().unwrap());
+        let right_size = self.get_size_or_zero(right.as_ref());
+        if self.first_is_to_the_left_of_second(node, root.as_ref().unwrap()) {
+            let decremented_left_size = left_size - T::funty(1);
+            if self.get_size_or_zero(self.get_right_or_default(right.as_ref())) > decremented_left_size {
+                self.left_rotate(root);
+            } else if self.get_size_or_zero(self.get_left_or_default(right.as_ref())) > decremented_left_size {
+                self.right_rotate(right);
+                self.left_rotate(root);
             } else {
-                if left_size > T::funty(0) && right_size > T::funty(0) {
-                    let replacement;
-                    if left_size > right_size {
-                        replacement = self.get_rightest(*left);
-                        self.detach_core(left, replacement);
-                    } else {
-                        replacement = self.get_leftest(*right);
-                        self.detach_core(right, replacement);
-                    }
-                    self.set_left(replacement, *left);
-                    self.set_right(replacement, *right);
-                    self.set_size(replacement, left_size + right_size);
-                    *root = replacement;
-                } else if left_size > T::funty(0) {
-                    *root = *left;
-                } else if right_size > T::funty(0) {
-                    *root = *right;
-                } else {
-                    *root = T::funty(0);
-                }
-                self.clear_node(node);
-                return;
+                self.dec_size(root.as_mut().unwrap());
+                root = left;
             }
+        } else if self.first_is_to_the_right_of_second(node, root.as_ref().unwrap()) {
+            let decremented_right_size = right_size - T::funty(1);
+            if self.get_size_or_zero(self.get_left_or_default(left.as_ref())) > decremented_right_size {
+                self.right_rotate(root);
+            } else if self.get_size_or_zero(self.get_right_or_default(left.as_ref())) > decremented_right_size {
+                self.left_rotate(left);
+                self.right_rotate(root);
+            } else {
+                self.dec_size(root.as_mut().unwrap());
+                root = right;
+            }
+        } else {
+            if left_size > T::funty(0) && right_size > T::funty(0) {
+                let replacement;
+                if left_size > right_size {
+                    replacement = self.get_rightest(left.as_ref().unwrap());
+                    self.detach_core(left, replacement);
+                } else {
+                    replacement = self.get_leftest(right.as_ref().unwrap());
+                    self.detach_core(right, replacement);
+                }
+                self.set_left(replacement, left.take().unwrap());
+                self.set_right(replacement, right.take().unwrap());
+                self.set_size(replacement, left_size + right_size);
+                *root = Some(replacement);
+            } else if left_size > T::funty(0) {
+                *root = left.take();
+            } else if right_size > T::funty(0) {
+                *root = right.take();
+            } else {
+                *root = None;
+            }
+            self.clear_node(node);
+            return;
         }
     }
+}
 }
